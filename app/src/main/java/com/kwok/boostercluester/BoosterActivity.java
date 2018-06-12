2f326eea0c4a40dcd39d31a2423a45482aede5d1
Jeremy Kwok <jerkwok@gmail.com>
2018-06-11 21:52:49 -0400

    write in update

---
+++ app/src/main/java/com/kwok/boostercluester/BoosterActivity.java
@@ -0,0 +1,101 @@
+package com.kwok.boostercluester;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BoosterActivity extends AppCompatActivity
+        implements View.OnClickListener, CardAdapter.CardClickListener{
+
+    private RecyclerView recyclerView;
+    private CardAdapter adapter;
+
+    MtgService mtgService;
+    Button button;
+    String cardListString = "";
+    String setString = "";
+    List<Card> cardList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booster_layout);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        mtgService = MtgService.retrofit.create(MtgService.class);
+
+        adapter = new CardAdapter(cardList, this);
+        recyclerView.setAdapter(adapter);
+
+        Intent intent = getIntent();
+        setString = intent.getStringExtra("SET");
+        if (setString.isEmpty()){
+            generateBooster("som");
+        }else{
+            generateBooster(setString);
+        }
+    }
+
+    private void generateBooster(String setId) {
+        cardListString = "";
+        Call<BoosterResponse> call = mtgService.generateBooster(setId);
+
+        call.enqueue(new Callback<BoosterResponse>() {
+            @Override
+            public void onResponse(Call<BoosterResponse> call, Response<BoosterResponse> response) {
+                BoosterResponse bodyList = response.body();
+
+                if(bodyList != null)
+                {
+                    cardList = bodyList.temp;
+                    adapter.setCardList(cardList);
+                    adapter.notifyDataSetChanged();
+                }else{
+                    Toast toast = Toast.makeText(BoosterActivity.this, "Set Not Found", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BoosterResponse> call, Throwable t) {
+                Log.d("Log", "log");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        generateBooster(setString);
+    }
+
+    @Override
+    public void onCardClicked(CardAdapter.ViewHolder cardViewHolder)
+    {
+        if (cardViewHolder.answerView.getVisibility() == View.GONE) {
+            cardViewHolder.answerView.setVisibility(View.VISIBLE);
+        }else{
+            cardViewHolder.answerView.setVisibility(View.GONE);
+        }
+    }
+}
--- app/src/main/java/com/kwok/boostercluester/MainActivity.java
+++ app/src/main/java/com/kwok/boostercluester/MainActivity.java
@@ -1,85 +1,36 @@
 package com.kwok.boostercluester;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import java.util.ArrayList;
-import java.util.List;
-
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener, CardAdapter.CardClickListener{
+        implements View.OnClickListener{
 
-    private RecyclerView recyclerView;
-    private CardAdapter adapter;
-
-    MtgService mtgService;
     Button button;
-    String cardListString = "";
-    List<Card> cardList = new ArrayList<>();
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recycler_view);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.generate_button);
         button.setOnClickListener(this);
-
-        mtgService = MtgService.retrofit.create(MtgService.class);
-
-        adapter = new CardAdapter(cardList, this);
-        recyclerView.setAdapter(adapter);
-
-        generateBooster("som");
-    }
-
-    private void generateBooster(String setId) {
-        cardListString = "";
-        Call<BoosterResponse> call = mtgService.generateBooster(setId);
-
-        call.enqueue(new Callback<BoosterResponse>() {
-            @Override
-            public void onResponse(Call<BoosterResponse> call, Response<BoosterResponse> response) {
-                BoosterResponse bodyList = response.body();
-
-                cardList = bodyList.temp;
-                adapter.setCardList(cardList);
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(Call<BoosterResponse> call, Throwable t) {
-                Log.d("Log", "log");
-            }
-        });
     }
 
     @Override
     public void onClick(View v) {
-        generateBooster("som");
-    }
-
-    @Override
-    public void onCardClicked(CardAdapter.ViewHolder cardViewHolder)
-    {
-        if (cardViewHolder.answerView.getVisibility() == View.GONE) {
-            cardViewHolder.answerView.setVisibility(View.VISIBLE);
-        }else{
-            cardViewHolder.answerView.setVisibility(View.GONE);
+        if (v.getId() == R.id.generate_button){
+            Intent intent = new Intent(this, BoosterActivity.class);
+            EditText editText = findViewById(R.id.set_edittext);
+            String set = editText.getText().toString();
+            if (!set.isEmpty()){
+                intent.putExtra("SET", set);
+                startActivity(intent);
+            }
         }
     }
 }
