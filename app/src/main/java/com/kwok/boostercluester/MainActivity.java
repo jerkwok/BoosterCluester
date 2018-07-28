0aacb86d630b358ffb2f587f83500e5e60e38fc3
Jeremy Kwok <jerkwok@gmail.com>
2018-07-28 10:38:46 -0400

    autocomplete sets, fixed keyboard

--- app/src/main/java/com/kwok/boostercluester/MainActivity.java
+++ app/src/main/java/com/kwok/boostercluester/MainActivity.java
@@ -1,17 +1,37 @@
 package com.kwok.boostercluester;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements View.OnClickListener{
 
     Button button;
+    AutoCompleteTextView autoCompleteTextView;
+
+    private static final String[] sets = new String[]{"DOM", "RIX", "XLN", "HOU", "AKH", "AER", "KLD", "KTK", "FRF", "DTK"};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +39,59 @@ public class MainActivity extends AppCompatActivity
 
         button = findViewById(R.id.generate_button);
         button.setOnClickListener(this);
+
+        autoCompleteTextView = findViewById(R.id.set_edittext);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, sets);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, readSets());
+
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    Log.e("TAG","Done pressed");
+                }
+                return false;
+            }
+        });
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                View view = getCurrentFocus();
+                if (view != null) {
+                    InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+            }
+        });
+    }
+
+    private ArrayList<String> readSets() {
+        ArrayList<String> sets = new ArrayList<>();
+
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("sets")));
+            String currSet;
+            while ((currSet = reader.readLine()) != null)
+            {
+                sets.add(currSet);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+        return sets;
     }
 
     @Override
